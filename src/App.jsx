d5518b3f0a14584dd4ff982384e30877f14933e0
AfrojaSultana44 <afrojasultanamoni311@gmail.com>
2024-04-16 12:46:32 +0600

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import './App.css'
+
+const routes = createBrowserRouter([
+  {
+
+  }
+])
+
+function App() {
+
+  return <RouterProvider router={routes}/>
+    
+}
+
+export default App
